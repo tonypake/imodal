fcfc05a73b8b934cd75091eea3e045dbb87fd299
cleverche <chenche0713@126.com>
2016-11-30 15:28:59 +0800

    test commit empty

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,13 +6,36 @@
  */
 //包含了整个Grunt配置信息
 module.exports = function(grunt) {
+	//js使用严格模式
+	'use strict';
+	
 	//初始化 configuration配置对象
 	grunt.initConfig({
 		//从package.json 文件读入项目配置信息，并存入pkg 属性内。这样就可以让我们访问到package.json文件中列出的属性了
 		pkg: grunt.file.readJSON('package.json'),
+		//文件开头的banner
+		banner: '/*!\n' +
+            ' * Dialog v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+            ' * Copyright 2016-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+            ' * Licensed under MIT (https://github.com/cleverche/dialog/blob/master/LICENSE)\n' +
+            ' */\n',
 		//任务配置
+		//清除内容任务
+		clean: {
+			dist: 'dist',
+			docs: 'docs/dist'
+		},
+		//connect服务器
+		connect: {
+			server: {
+				options: {
+					port: 3000,
+					base: '.'
+				}
+			}
+		},
 		jshint: {
-		  files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+		  files: ['Gruntfile.js', 'src/**/*.js', 'examples/**/*.js'],
 		  options: {
 			globals: {
 			  jQuery: true
